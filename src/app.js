e68e211ac9a129fa995f56c96ac94b9485a85d50
Yann <106726503+YannGB-c@users.noreply.github.com>
2025-07-21 19:46:06 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let pronouns = ['the', 'our']
+let adjs = ['great', 'big' ]
+let nouns = ['jogger' , 'racoon']
+const domains = [];
+
+
+function DomainGenerator(pronouns,adjs,nouns) {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+for (let pronoun of pronouns){
+  for (let adj of adjs){
+    for (let noun of nouns){
+      const domain = `${pronoun}${adj}${noun}.com`
+      domains.push(domain);
+    }
+  }
+}
+
+return domains;
+
+
+}
+
+const domainlist = DomainGenerator(pronouns,adjs,nouns);
+domainlist.forEach(domain => console.log(domain));
+
